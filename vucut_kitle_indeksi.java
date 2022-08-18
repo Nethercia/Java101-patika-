1ba7891f7460d32a784030ea83cd228f29d104b9
Yiğit <35572825+Nethercia@users.noreply.github.com>
2022-08-19 00:11:41 +0300

    Add files via upload

---
+++ vucut_kitle_indeksi.java
@@ -0,0 +1,19 @@
+package java101;
+
+import java.util.Scanner;
+
+public class vucut_kitle_indeksi {
+    public static void main(String[] args) {
+        double boy,kilo,indeks;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Boyunuzu giriniz : ");
+        boy = inp.nextDouble();
+        System.out.println("Kilonuzu giriniz : ");
+        kilo = inp.nextDouble();
+        indeks = kilo/(boy*boy);
+        System.out.println("Kilonuz : "+kilo);
+        System.out.println("Boyunuz : "+boy);
+        System.out.println("Vücut kitle indeksiniz : "+indeks);
+
+    }
+}
