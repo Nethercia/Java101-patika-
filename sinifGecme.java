f316758eadd2ae4cff7b22fab6cae9bf7a0e4a67
Yiğit <35572825+Nethercia@users.noreply.github.com>
2022-08-20 19:27:49 +0300

    Update sinifGecme.java

--- sinifGecme.java
+++ sinifGecme.java
@@ -7,6 +7,7 @@ public class sinifGecme {
         int mat, turkce, fizik, kimya, muzik;
         double ortalama;
         int toplamNot = 0;
+        int toplamDers = 0;
 
         Scanner inp = new Scanner(System.in);
 
@@ -14,33 +15,38 @@ public class sinifGecme {
         mat = inp.nextInt();
         if (!(mat <= 0 || mat > 100)){
             toplamNot += mat;
+            toplamDers++;
         }
 
         System.out.println("Türkçe notunuz : ");
         turkce = inp.nextInt();
         if (!(turkce <= 0 || turkce > 100)){
             toplamNot += turkce;
+            toplamDers++;
         }
 
         System.out.println("Fizik notunuz : ");
         fizik = inp.nextInt();
         if (!(fizik <= 0 || fizik > 100)){
             toplamNot += fizik;
+            toplamDers++;
         }
 
         System.out.println("Kimya notunuz : ");
         kimya = inp.nextInt();
         if (!(kimya <= 0 || kimya > 100)){
             toplamNot += kimya;
+            toplamDers++;
         }
 
         System.out.println("Müzik notunuz : ");
         muzik = inp.nextInt();
         if (!(muzik <= 0 || muzik > 100)){
             toplamNot += muzik;
+            toplamDers++;
         }
 
-        ortalama = toplamNot / 5;
+        ortalama = toplamNot / toplamDers;
 
         if (ortalama <= 55){
             System.out.println("Kaldınız...");
