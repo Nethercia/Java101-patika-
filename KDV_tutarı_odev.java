63d1420ee1195d3a08deebd9508c9668f3cff26b
Yiğit <35572825+Nethercia@users.noreply.github.com>
2023-03-23 17:08:05 +0300

    Update KDV_tutarı_odev.java

--- KDV_tutarı_odev.java
+++ KDV_tutarı_odev.java
@@ -2,7 +2,6 @@ package java101;
 
 import java.util.Scanner;
 /*
-Ödev
 Eğer girilen tutar 0 ve 1000 TL arasında
  ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını
  %8 olarak KDV tutarı hesaplayan programı yazınız.
